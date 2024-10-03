fb2d4cc842ecf430d929f0b9095ae3021cc1d538
Eduardo <eduardo25082002@gmail.com>
2024-10-03 18:57:58 -0300

    Commit For

---
+++ Repeticao/src/For.java
@@ -0,0 +1,22 @@
+public class For { // Declarei a minha classe "FOR"
+    public static void main(String[] args){ // Criei a minha main para executar o código
+        for( int pixels = 1; pixels <= 22; pixels++){ // Criação do método "FOR", declarando Var, comparando valor e adicionando contador.
+            System.out.println("A quantidade de pixels é: " + pixels); // Imprimo a mensagem de texto, com o resultado do meu loop.
+        }
+        System.out.println("A quantidade de pixels, agradou a todos"); // Mensagem de finalização (Opcional)
+
+        /*
+         * int pixels = 1 -> bloco de inicialização
+         * pixels <= 22 -> expressão booleana de validação
+         * pixels++ -> bloco de atualização
+         *
+         * OBS: sempre separado por ponto e virgula (;)
+         * 
+         * for(int pixels = 1; pixels <= 22; pixels++)
+         * O comando vai ser executado até que afirmação se torne falsa
+         * Enquanto a VAR for menor, ela vai executar o contador(incrementação unária).
+         */
+
+
+    }
+} // Estrutura de Repetição com FOR
